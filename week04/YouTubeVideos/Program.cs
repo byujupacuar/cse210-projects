0a1e9bd75e04988b8b17ad86531b8149b534c9f3
byujupacuar <jcuizaarce@byupathway.edu>
2025-06-02 21:55:23 -0400

    week04

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    private List<Video> videos;
+
+    public Program()
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        videos = new List<Video>();
+    }
+
+    public void InitializeVideos()
+    {
+        Video video1 = new Video("Learning C#", "Juan Pérez", 300);
+        video1.Comments.Add(new Comment("María", "Very useful, thank you!"));
+        video1.Comments.Add(new Comment("Carlos", "Great explanation!"));
+
+        Video video2 = new Video("Design Patterns", "Ana López", 450);
+        video2.Comments.Add(new Comment("Luis", "Incredible content!"));
+
+        videos.Add(video1);
+        videos.Add(video2);
+    }
+
+    public void DisplayVideos()
+    {
+        foreach (var video in videos)
+        {
+            video.DisplayInfo();
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        Program program = new Program();
+        program.InitializeVideos();
+        program.DisplayVideos();
     }
 }
